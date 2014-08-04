3db3101bb16eb15c97eb56eae08a9d62813fa185
Kyle Drake <kyle@kyledrake.net>
2014-08-04 14:32:42 -0700

    update example

--- test.js
+++ test.js
@@ -12,8 +12,12 @@ fiat.on('escrow', function(billType) {
   if(amount == 5) {
     console.log("Sorry, we don't accept $5 bills!")
     fiat.reject()
-  } else
+  } else {
     fiat.stack()
+  }
+
+  // Call when done:
+  // fiat.stop()
 })
 
 fiat.on('error', function(type) {
@@ -21,22 +25,3 @@ fiat.on('error', function(type) {
   console.log('ERROR: '+type)
   fiat.accept()
 })
-
-/*
-
-fiat.on('open', function() {
-  fiat.accept()
-})
-
-fiat.on('escrow', function(amount) {
-  fiat.stack()
-  // woo awesome let's get some more
-  fiat.accept()
-})
-
-fiat.on('error', function(type) {
-  console.log(type)
-  fiat.refund()
-})
-
-*/
